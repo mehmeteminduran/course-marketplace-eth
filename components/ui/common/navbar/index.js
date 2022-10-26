5c9f4ccd8e29656271de1cd4abf705253571e76e
“memin.duran” <memin.duran@stm.com.tr>
2022-10-26 10:11:31 +0300

    web3 provider added, directory structure changed

--- components/ui/common/navbar/index.js
+++ components/ui/common/navbar/index.js
@@ -1,11 +1,18 @@
+import { useWeb3 } from "@components/providers";
 import Link from "next/link";
+import Button from "../button";
+import {useAccount} from "@components/web3/hooks/useAccount"
 
 export default function Navbar() {
+  const { connect, isWeb3Loaded, isLoading } = useWeb3();
+  const { account } = useAccount()
+
   return (
     <section>
+      {account}
       <div className="relative pt-6 px-4 sm:px-6 lg:px-8">
         <nav className="relative" aria-label="Global">
-          <div className="flex justify-between">
+          <div className="flex justify-between items-center">
             <div>
               <Link href="/">
                 <a className="font-medium mr-8 text-gray-500 hover:text-gray-900">
@@ -29,12 +36,13 @@ export default function Navbar() {
                   Wishlist
                 </a>
               </Link>
-              <a
-                href="#"
-                className="px-8 py-3 border rounded-md text-base font-medium text-white bg-indigo-600 hover:bg-indigo-500"
-              >
-                Connect Wallet
-              </a>
+              {isLoading ? (
+                <Button disabled={true} onClick={connect}>Loading...</Button>
+              ) : isWeb3Loaded ? (
+                <Button onClick={connect}>Connect Wallet</Button>
+              ) : (
+                <Button onClick={() => window.open("https://metamask.io/download/","_blank")}>Install Metamask</Button>
+              )}
             </div>
           </div>
         </nav>
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,14 @@
-import {Hero, Breadcrumbs} from "@components/common";
-import { CourseList } from "@components/course";
-import { BaseLayout } from "@components/layout";
-import { OrderCard } from "@components/order";
-import { EthRates, WalletBar } from "@components/web3";
+import { useWeb3 } from "@components/providers";
+import {Hero, Breadcrumbs} from "@components/ui/common";
+import { CourseList } from "@components/ui/course";
+import { BaseLayout } from "@components/ui/layout";
+import { OrderCard } from "@components/ui/order";
+import { EthRates, WalletBar } from "@components/ui/web3";
 import { getAllCourses } from "content/courses/fetcher";
 
-export default function Home({courses}) {
+export default function Home({courses}) { 
   return (  
-    <>
+    <> 
             <Hero/>
             <Breadcrumbs/>
             <WalletBar />

759faf06c29168dc83a96fa06f9529902e9a84d7
“memin.duran” <memin.duran@stm.com.tr>
2022-11-15 14:06:11 +0300

    Marketplace contract added

---
+++ contracts/CourseMarketplace.sol
@@ -0,0 +1,107 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract CourseMarketplace {
+    enum State {
+        Purchased,
+        Activated,
+        Deactivated
+    }
+
+    struct Course {
+        uint256 id;
+        uint256 price;
+        bytes32 proof;
+        address owner;
+        State state;
+    }
+
+    // CourseHash to Course data
+    mapping(bytes32 => Course) private ownedCourses;
+
+    // CourseId to CourseHash
+    mapping(uint256 => bytes32) private ownedCourseHash;
+
+    // number of all courses + id of the course
+    uint256 private totalOwnedCourses;
+
+    address payable private owner;
+
+    constructor() {
+        setContractOwner(msg.sender);
+    }
+
+    /// Course has already a Owner
+    error CourseHasOwner();
+
+    /// Course Owner has an access
+    error OnlyOwner();
+
+    modifier onlyOwner() {
+        if (msg.sender != getContractOwner()) {
+            revert OnlyOwner();
+        }
+        _;
+    }
+
+    function purchaseCourse(bytes16 courseId, bytes32 proof) external payable {
+        // courseId : 10
+        // courseIdHex: 0x00000000000000000000000000003130
+        // proof: 0x0000000000000000000000000000313000000000000000000000000000003130
+        bytes32 courseHash = keccak256(abi.encodePacked(courseId, msg.sender));
+        if (hasCourseOwnership(courseHash)) {
+            revert CourseHasOwner();
+        }
+
+        uint256 id = totalOwnedCourses++;
+
+        ownedCourseHash[id] = courseHash;
+        ownedCourses[courseHash] = Course({
+            id: id,
+            price: msg.value,
+            proof: proof,
+            owner: msg.sender,
+            state: State.Purchased
+        });
+    }
+
+    function transferOwnership(address newOwner) external onlyOwner {
+        setContractOwner(newOwner);
+    }
+
+    function getCourseCount() external view returns (uint256) {
+        return totalOwnedCourses;
+    }
+
+    function getCourseHashAtIndex(uint256 index)
+        external
+        view
+        returns (bytes32)
+    {
+        return ownedCourseHash[index];
+    }
+
+    function getCourseByHash(bytes32 courseHash)
+        external
+        view
+        returns (Course memory)
+    {
+        return ownedCourses[courseHash];
+    }
+
+    function getContractOwner() public view returns (address) {
+        return owner;
+    }
+
+    function setContractOwner(address newOwner) private {
+        owner = payable(newOwner);
+    }
+
+    function hasCourseOwnership(bytes32 courseHash)
+        private
+        view
+        returns (bool)
+    {
+        return ownedCourses[courseHash].owner == msg.sender;
+    }
+}
